d4e721e4fdd3ecd9317c7a3b8831cde9fa8262af
sdilonedev <dilonealcantara@gmail.com>
2024-11-07 12:30:55 -0400

    Add hero component into the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/layout/hero";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen">
+      <Hero />
     </main>
   );
 }
--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -1,11 +1,25 @@
+import { Button } from "@/ui/button";
+import GithubIcon from "../icons/github";
+
 export default function Hero() {
   return (
     <section className="relative flex w-full flex-col items-center justify-start px-4 pt-32 sm:px-6 sm:pt-24 md:pt-32 lg:px-8">
-      <div className="flex flex-col items-center text-center space-y-4">
-        <h1 className="text-5xl font-bold my-4 max-w-xl">Enhance Your Link Management</h1>
+      <div className="flex flex-col items-center text-center space-y-6">
+        <h1 className="text-5xl font-bold my-4 max-w-xl">
+          Take Control of Your Links Like a Pro
+        </h1>
         <p className="text-base max-w-xl">
-          Slug is an open-source platform that allows you to create, manage, and share short links with ease. It’s fast, secure, and easy to use.
+          Slash is an open-source project by SDilone designed to provide an efficient solution for managing and sharing short links. With its fast, secure, and user-friendly interface, Slash makes link management simpler than ever.
         </p>
+        <div className="flex flex-row justify-center gap-4 items-center">
+          <Button size="lg">
+            Get started
+          </Button>
+          <Button variant="outline" size="lg">
+            <GithubIcon />
+            Source code
+          </Button>
+        </div>
       </div>
     </section>
 
