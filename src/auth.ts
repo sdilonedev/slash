533850e2e301c9bee34ee0cf76211b582b8e1f01
sdilonedev <dilonealcantara@gmail.com>
2024-11-07 23:34:53 -0400

    Add next auth configurations

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import db from "./server/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+  unstable_update
+} = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    })
+  ],
+  session: {
+    strategy: "jwt"
+  },
+  adapter: PrismaAdapter(db),
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/auth",
+    error: "/auth/error"
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      console.log(user, account)
+      return true;
+    }
+  }
+
+})
