c3aeb1ef7fc0296aff82182f63f8f61d0d9c6995
sdilonedev <dilonealcantara@gmail.com>
2024-11-11 22:52:42 -0400

    Fix missing header in auth page

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,19 @@
+import Header from "@/components/layout/header";
+
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 export default function AuthLayout(props: AuthLayoutProps) {
   return (
-    <main className="flex flex-col w-full justify-center space-y-8 mt-11">
-      <div className="flex flex-col items-center justify-center">
-        {props.children}
-      </div>
-    </main>
+    <>
+      <Header />
+
+      <main className="flex flex-col w-full justify-center space-y-8 mt-11">
+        <div className="flex flex-col items-center justify-center">
+          {props.children}
+        </div>
+      </main>
+    </>
   )
 }
