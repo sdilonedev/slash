364d5ed00bac7891ba065865f5afd8634184057c
sdilonedev <dilonealcantara@gmail.com>
2024-11-11 22:54:15 -0400

    Reformat component to remove unnecessary whitespace

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,18 +9,14 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   return (
     <>
       <DashHeader />
-
-      <nav className="fixed z-50 w-full bg-white dark:bg-neutral-900 border-b border-neutral-200 dark:border-neutral-800 shadow-sm backdrop-blur-md">
-        <div className="w-full mx-auto px-4">
+      <nav className="fixed z-50 flex w-full items-center border-b border-neutral-200 bg-white shadow-sm backdrop-blur-md dark:border-neutral-800 dark:bg-neutral-900">
+        <div className="w-full mx-auto container">
           <div className="flex justify-between items-center">
             <div className="flex items-center space-x-4 text-sm font-medium rtl:space-x-reverse">
               <DashBreadcrumbs />
-
             </div>
-
             <div className="hidden md:flex items-center space-x-2">
               <LinkLimit userLinks={29} maxLinks={30} />
-
               <CreateLink>
                 <Button variant="outline">
                   <PlusIcon size={15} />
@@ -31,8 +27,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
           </div>
         </div>
       </nav>
-
-      <main className="flex mx-auto my-16 w-full max-w-screen-xl px-4 items-center">
+      <main className="flex mx-auto my-16 w-full items-center container">
         {children}
       </main>
     </>
